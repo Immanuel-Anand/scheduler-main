474cca66736e8931d9ffc1db9a8d8a54133114f2
Immanuel-Anand <immanuel-anand@outlook.com>
2025-02-24 21:10:23 +0530

    typescript parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,24 @@
 module.exports = {
   extends: [
     'next/core-web-vitals',
-    'eslint:recommended'
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   rules: {
     'no-unused-vars': 'warn',
-    'no-console': 'warn'
+    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn'
   },
-  parser: '@babel/eslint-parser',
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true
     },
-    requireConfigFile: false
+    project: './tsconfig.json'
   },
+  plugins: ['@typescript-eslint'],
   env: {
     browser: true,
     node: true,
